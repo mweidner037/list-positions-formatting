e5c9ab66627b60a3612718ff87527e14fb9cb35b
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2023-12-21 12:13:57 -0500

    default Lamport order Formatting

---
+++ src/formatting.ts
@@ -0,0 +1,67 @@
+import { BunchIDs, Order } from "list-positions";
+import {
+  AbstractFormatting,
+  AbstractSpan,
+  Anchor,
+  FormatChange,
+} from "./abstract_formatting";
+
+// Allow "any" as the span value type.
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+export type Span = {
+  start: Anchor;
+  end: Anchor;
+  key: string;
+  /** Anything except null - that's reserved to mean "delete this format". */
+  value: any;
+  creatorID: string;
+  /** Lamport timestamp. Ties broken by creatorID. Always positive. */
+  timestamp: number;
+};
+
+export class Formatting extends AbstractFormatting<Span> {
+  readonly replicaID: string;
+  private lamport = 0;
+
+  constructor(order: Order, replicaID?: string) {
+    super(order);
+
+    this.replicaID = replicaID ?? BunchIDs.newReplicaID();
+  }
+
+  protected compareSpans(a: Span, b: Span): number {
+    // Lamport timestamp order, with ties broken by creatorID.
+    if (a.timestamp !== b.timestamp) return a.timestamp - b.timestamp;
+    if (a.creatorID === b.creatorID) return 0;
+    return a.creatorID > b.creatorID ? 1 : -1;
+  }
+
+  protected newSpan(base: AbstractSpan): Span {
+    this.lamport++;
+    return {
+      ...base,
+      creatorID: this.replicaID,
+      timestamp: this.lamport,
+    };
+  }
+
+  // Overrides to track Lamport timestamp.
+
+  addSpan(span: Span): FormatChange[] {
+    this.lamport = Math.max(this.lamport, span.timestamp);
+    return super.addSpan(span);
+  }
+
+  load(savedState: Span[]): void {
+    // Since savedState is in order, the last timestamp is the greatest.
+    if (savedState.length !== 0) {
+      this.lamport = Math.max(
+        this.lamport,
+        savedState[savedState.length - 1].timestamp
+      );
+    }
+    super.load(savedState);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from "./abstract_formatting";
+export * from "./formatting";

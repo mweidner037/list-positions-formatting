ad9dbea84a97ee52a123460793356bb0b2849501
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2023-12-31 16:15:41 -0500

    anchor and helper docs

--- src/anchor.ts
+++ src/anchor.ts
@@ -1,32 +1,66 @@
 import { LexList, List, Order, Outline, Position } from "list-positions";
 
+/**
+ * An anchor in a list, as a JSON object.
+ *
+ * An *anchor* is a spot immediately before or after a
+ * [position](https://github.com/mweidner037/list-positions#about).
+ * See TODO: readme picture.
+ *
+ * Each formatting mark starts and ends at an anchor.
+ * Using anchors instead of positions lets the mark choose whether it "expands"
+ * to include new positions before/after its original range.
+ * 
+ * See also: [Utilities](TODO:readme section) for working with Anchors.
+ */
 export type Anchor = {
   /**
-   * Could be min or max position, but marks can't include them.
-   * So be careful allowing min/max Positions in your list.
+   * The anchor's Position, from the [list-positions](https://github.com/mweidner037/list-positions)
+   * library.
    */
   pos: Position;
   /**
-   * true if before, false if after.
+   * True for a "before" anchor, false for an "after" anchor.
    */
   before: boolean;
 };
 
 export const Anchors = {
+  /**
+   * The minimum Anchor, which is after Order.MIN_POSITION.
+   */
   MIN_ANCHOR: { pos: Order.MIN_POSITION, before: false } as Anchor,
 
+  /**
+   * The maximum Anchor, which is before Order.MAX_POSITION.
+   */
   MAX_ANCHOR: { pos: Order.MAX_POSITION, before: true } as Anchor,
 
+  /**
+   * Returns whether two Anchors are equal, i.e., they have equal contents.
+   */
   equals(a: Anchor, b: Anchor): boolean {
     return a.before === b.before && Order.equalsPosition(a.pos, b.pos);
   },
 
   /**
-   * Returns the next index after anchor in list,
+   * Returns the next index to the right of anchor in the given list,
    * or `list.length` if anchor is after all present positions.
-   *
-   * You can use this function to convert either endpoint of a span
-   * to the corresponding slice endpoint (see sliceFromSpan).
+   * 
+   * A span `{ start: Anchor, end: Anchor }`, when projected onto a list,
+   * covers precisely the slice 
+   * ```ts
+   * { 
+   *   startIndex: Anchors.indexOfAnchor(list, start),
+   *   endIndex: Anchors.indexOfAnchor(list, end)
+   * }
+   * ```
+   * (endIndex not included).
+   * 
+   * See also:
+   * - sliceFromSpan: Does the above start/end to slice conversion.
+   * - spanFromSlice: Partial inverse for sliceFromSpan, and the closest thing
+   * to an inverse for this function.
    */
   indexOfAnchor(
     list: List<unknown> | LexList<unknown> | Outline,
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,31 @@
 import { LexList, List, Order, Outline } from "list-positions";
 import { Anchor, Anchors } from "./anchor";
 
+// Helper functions.
+// See RichList for example usage.
+
+/**
+ * Returns a span `{ start: Anchor, end: Anchor }` that covers precisely
+ * the given slice of list. The startIndex and endIndex are as in `Array.slice`.
+ * 
+ * The span covers all positions from
+ * `list.positionAt(startIndex)` to `list.positionAt(endIndex - 1)` inclusive,
+ * including positions that are not currently present in list.
+ * It may also "expand" to cover not-currently-present positions at
+ * the slice's endpoints, depending on the value of `expand`.
+ * 
+ * @param expand How the span affects not-currently-present positions at
+ * the slice's endpoints.
+ * - "after" (default): The span expands to cover positions at the end, i.e.,
+ * between `list.positionAt(endIndex - 1)` and `list.positionAt(endIndex)`.
+ * This is how most marks (e.g. bold) usually behave in rich-text editors.
+ * - "before": Expands to cover positions at the beginning, i.e.,
+ * between `list.positionAt(startIndex - 1)` and `list.positionAt(startIndex)`
+ * - "both": Combination of "after" and "before".
+ * - "none": Does not expand. This is how hyperlinks usually behave in rich-text editors.
+ * 
+ * @throws If startIndex >= endIndex (the slice is empty).
+ */
 export function spanFromSlice(
   list: List<unknown> | LexList<unknown> | Outline,
   startIndex: number,
@@ -40,6 +65,13 @@ export function spanFromSlice(
 
 // Note: might return trivial slice (same start and end).
 // But spanFromSlice won't accept that. TODO: accept it?
+/**
+ * Projects the span `{ start: Anchor, end: Anchor }` onto the given list,
+ * returning the slice that it currently covers.
+ *
+ * The slice is expressed in terms of its startIndex and endIndex
+ * (endIndex not included), like arguments to `Array.slice`.
+ */
 export function sliceFromSpan(
   list: List<unknown> | LexList<unknown> | Outline,
   start: Anchor,
@@ -52,9 +84,14 @@ export function sliceFromSpan(
 }
 
 /**
- * Returns changes (including null for deletions) to turn current into target.
+ * Returns a map of format changes needed to turn `format` into `current`.
  *
- * null values are ignored (treated as not present).
+ * Usually, you will create a new mark for each key-value pair in the returned map.
+ * Note that the map may contain null
+ * values; when used in marks, these delete their keys.
+ *
+ * See also: RichList.insertWithFormat, which uses this function to ensure that
+ * newly-inserted values have the desired format.
  */
 export function diffFormats(
   current: Record<string, any>,
